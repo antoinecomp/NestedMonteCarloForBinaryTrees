48c727ea3cb35f87f3d273a9f078f2861c098b49
Your Name <antoine.compagnie@gmail.com>
2018-02-18 14:55:42 +0100

    tried to create class Move

--- main2.cpp
+++ main2.cpp
@@ -1,6 +1,5 @@
 #define NULL 0
-#include "nestedSimple.c"
-#include "Board.h"
+#include <vector>
 
 // main2
 
@@ -111,6 +110,149 @@ void btree::destroy_tree()
 {
   destroy_tree(root);
 }
+
+class Board{
+
+    public:
+        Board();
+        ~Board();
+
+		//a Board is made from a binary tree. Furthermore he has a score, a length
+		Board(btree,node);
+
+		//And he has possible blows
+		legalMoves(node); // where we can go from node
+
+    private:
+		//member variables
+		int length;
+		int score;
+		node; // Where we are at the moment of the board
+		// create move ? Array of the following possibilities : left/right ? Or none if we reached the end
+		vector<*node> moves(2,NULL);
+		
+
+};
+
+Board::Board(){
+	length = 0;
+	score = 0;
+
+}; 
+
+Board::~Board(){
+
+}; 
+
+Board::Board(btree b, node n){
+	length = NULL; //TO-DO : number of nodes which have leaves BUT how to count them ?
+
+	score = n.key_value; //DONE? : number on the current node FROM node
+	
+	if(n->*left != NULL)
+		moves[0] = n->*left;//DONE? array of the left and right positions from current state 	//node *left; and node *right;
+		moves[1] = n->*right;
+	
+}; 
+
+// gives the number of leaves we can play : either 2 or 0 in the binary-tree case.
+int Board::legalMoves(Move moves){
+	//If there is leaves there is at least two moves.
+	if(n->*left != NULL){// wrong we have to take move into account
+		return 2;	
+	}//otherwise there is no possibilities
+	else{
+		return 0;
+	}
+	
+};
+
+/**/
+double playout (Board * board) {
+  Move moves [MaxLegalMoves];
+  while (true) {
+    int nb = board->legalMoves (moves);
+    if ((nb == 0) || board->terminal ())
+      return board->score ();
+    int n = rand () % nb;
+    board->play (moves [n]);
+    if (board->length >= MaxPlayoutLength - 20) {
+      return 0;
+    }
+  }
+};
+
+//it's an array of possible moves. It takes node left and node right from a given node in a Board
+class Move{
+    public:
+        Move();
+        ~Move();
+
+    private:
+		vector<node> moves[2] 
+};
+  
+#include <float.h>
+
+double bestScoreNested = -DBL_MAX;
+
+int lengthBestRollout [10];
+double scoreBestRollout [10];
+Move bestRollout [10] [MaxPlayoutLength];
+
+Board bestBoard;
+
+// What is int n ?
+double nested (Board & board, int n) {
+  int nbMoves = 0;
+  Move moves [MaxLegalMoves];
+
+  lengthBestRollout [n] = -1;
+  scoreBestRollout [n] = -DBL_MAX;
+  float res;
+  while (true) {
+    if (board.terminal ())
+      return 0.0;
+      //return board.score ();
+    nbMoves = board.legalMoves (moves);
+    for (int i = 0; i < nbMoves; i++) {
+      Board b = board;
+      b.play (moves [i]);
+		//what is n ?
+      if (n == 1)
+	playout (&b);
+      else
+	nested (b, n - 1);     
+      double score = b.score ();
+      if (score > scoreBestRollout [n]) {
+	scoreBestRollout [n] = score;
+	lengthBestRollout [n] = b.length;
+	for (int k = 0; k < b.length; k++)
+	  bestRollout [n] [k] = b.rollout [k];
+	// what is n ?
+	if (n > 3) {
+	  for (int t = 0; t < n - 1; t++)
+	    fprintf (stderr, "\t");
+	  fprintf (stderr, "n = %d, progress = %d, score = %f\n", n, board.length, scoreBestRollout [n]);
+          int depth = 0;
+          b.print (stderr);
+	  fprintf (stderr, "\n");
+	}
+	if ((n > 1) && (score > bestScoreNested)) {
+	  bestScoreNested = score;
+	  fprintf (stderr, "best score = %f\n", score);
+          b.print (stderr);
+	  fprintf (stderr, "\n");
+          bestBoard = b;
+        }
+      }
+    }
+    board.play (bestRollout [n] [board.length]);
+  }
+  return 0.0;
+};
+
+/**/
 
 int main(int argc, char *argv []) {
     btree b;
