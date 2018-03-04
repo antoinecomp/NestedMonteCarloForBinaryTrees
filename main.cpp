d8d2ffdfa00cf28c51290eb367cf3c43106f85b4
Your Name <antoine.compagnie@gmail.com>
2018-03-04 14:23:20 +0100

    indentation

--- main.cpp
+++ main.cpp
@@ -112,6 +112,7 @@ void btree::destroy_tree()
   destroy_tree(root);
 }
 
+// Displ
 class Board{
 
     public:
@@ -178,9 +179,11 @@ class Move{
 		vector<node> moves[2];
 };
 
-Move::Move{
-	
+Move::Move(){
+	moves= NULL;
 };
+
+
   
 
 /**/
@@ -211,51 +214,52 @@ Board bestBoard;
 
 // What is int n ?
 double nested (Board & board, int n) {
-  int nbMoves = 0;
-  Move moves [MaxLegalMoves];
+	int nbMoves = 0;
+	Move moves [MaxLegalMoves];
 
-  lengthBestRollout [n] = -1;
-  scoreBestRollout [n] = -DBL_MAX;
-  float res;
-  while (true) {
-    if (board.terminal ())
-      return 0.0;
-      //return board.score ();
-    nbMoves = board.legalMoves (moves);
-    for (int i = 0; i < nbMoves; i++) {
-      Board b = board;
-      b.play (moves [i]);
-		//what is n ?
-      if (n == 1)
-	playout (&b);
-      else
-	nested (b, n - 1);     
-      double score = b.score ();
-      if (score > scoreBestRollout [n]) {
-	scoreBestRollout [n] = score;
-	lengthBestRollout [n] = b.length;
-	for (int k = 0; k < b.length; k++)
-	  bestRollout [n] [k] = b.rollout [k];
-	// what is n ?
-	if (n > 3) {
-	  for (int t = 0; t < n - 1; t++)
-	    fprintf (stderr, "\t");
-	  fprintf (stderr, "n = %d, progress = %d, score = %f\n", n, board.length, scoreBestRollout [n]);
-          int depth = 0;
-          b.print (stderr);
-	  fprintf (stderr, "\n");
+	lengthBestRollout [n] = -1;
+	scoreBestRollout [n] = -DBL_MAX;
+	float res;
+	while (true) {
+		// if there is no more moves to play ?
+		if (board.terminal ())
+			return 0.0;
+			//return board.score();
+		// if there is still things to play
+		nbMoves = board.legalMoves (moves);
+		for (int i = 0; i < nbMoves; i++) {
+			Board b = board;
+			b.play(moves[i]);
+		  	if (n == 1)
+				playout (&b);
+		  	else
+				nested (b, n - 1);     
+				double score = b.score ();
+		    if (score > scoreBestRollout [n]) {
+				scoreBestRollout [n] = score;
+				lengthBestRollout [n] = b.length;
+				for (int k = 0; k < b.length; k++)
+		  			bestRollout [n] [k] = b.rollout [k];
+				// what is n ?
+				if (n > 3) {
+					for (int t = 0; t < n - 1; t++)
+						fprintf (stderr, "\t");
+				  	fprintf (stderr, "n = %d, progress = %d, score = %f\n", n, board.length, scoreBestRollout [n]);
+					int depth = 0;
+					b.print (stderr);
+					fprintf (stderr, "\n");
+				}
+				if ((n > 1) && (score > bestScoreNested)) {
+					bestScoreNested = score;
+					fprintf (stderr, "best score = %f\n", score);
+					b.print (stderr);
+					fprintf (stderr, "\n");
+					bestBoard = b;
+				}
+			}
+		}
+		board.play (bestRollout [n] [board.length]);
 	}
-	if ((n > 1) && (score > bestScoreNested)) {
-	  bestScoreNested = score;
-	  fprintf (stderr, "best score = %f\n", score);
-          b.print (stderr);
-	  fprintf (stderr, "\n");
-          bestBoard = b;
-        }
-      }
-    }
-    board.play (bestRollout [n] [board.length]);
-  }
   return 0.0;
 };
 
